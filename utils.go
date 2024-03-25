8b97edb2d83d9eeae4c9f31ee44342c7a13ec203
syscrypt <syscrypt@protonmail.com>
2024-03-25 14:21:18 +0100

    feat(utils): implemented method for request path inference

--- utils.go
+++ utils.go
@@ -1,15 +1,21 @@
 package sgwf
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"reflect"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 var (
-	ErrValueNotFound = errors.New("couldn't find any path variables")
+	ErrValueNotFound  = errors.New("couldn't find any path variables")
+	ErrNoStructParam  = errors.New("provided parameter is not of type struct")
+	ErrWrongJsonTag   = errors.New("provided struct properties must have single name json tags")
+	ErrNonPointerType = errors.New("parameter must be a pointer")
 )
 
 func CheckVars(r *http.Request, vars ...string) error {
@@ -26,4 +32,57 @@ func CheckVars(r *http.Request, vars ...string) error {
 	}
 
 	return nil
+}
+
+func PackRequestPathVariables(r *http.Request, dest interface{}) error {
+	values := mux.Vars(r)
+
+	if values == nil {
+		return ErrValueNotFound
+	}
+
+	if reflect.TypeOf(dest).Kind() != reflect.Pointer {
+		return ErrNonPointerType
+	}
+
+	types, err := getStructTypes(reflect.ValueOf(dest).Elem().Interface())
+	if err != nil {
+		return err
+	}
+
+	result := []string{}
+	for k, v := range values {
+		if types[k] != "string" {
+			result = append(result, fmt.Sprintf("\"%s\": %s", k, v))
+		} else {
+			result = append(result, fmt.Sprintf("\"%s\": \"%s\"", k, v))
+		}
+	}
+
+	resultStr := strings.Join(result, ",")
+	resultStr = fmt.Sprintf("{%s}", resultStr)
+
+	return json.Unmarshal([]byte(resultStr), dest)
+}
+
+func getStructTypes(i interface{}) (map[string]string, error) {
+	val := reflect.ValueOf(i)
+	typ := val.Type()
+	ret := make(map[string]string)
+
+	if val.Kind() != reflect.Struct {
+		return nil, ErrNoStructParam
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		fieldType := typ.Field(i)
+		fieldJsonName := fieldType.Tag.Get("json")
+		if fieldJsonName == "" || len(strings.Split(fieldJsonName, ",")) > 1 {
+			return nil, ErrWrongJsonTag
+		}
+
+		ret[fieldJsonName] = fieldType.Type.Name()
+	}
+
+	return ret, nil
 }
--- utils_test.go
+++ utils_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type dest struct {
+	Param1 int    `json:"param1"`
+	Param2 bool   `json:"param2"`
+	Param3 string `json:"param3"`
+}
+
 func TestCheckVarsValidParam(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/test/1234", nil)
 	req = mux.SetURLVars(req, map[string]string{
@@ -26,4 +32,59 @@ func TestCheckVarsInvalidParam(t *testing.T) {
 
 	err := CheckVars(req, "param1", "param2")
 	assert.Equal(t, "couldn't find path variable param2 in request path", err.Error())
+}
+
+func TestStructTypesReturnsNilOnWrongType(t *testing.T) {
+	_, err := getStructTypes(10)
+	assert.ErrorIs(t, ErrNoStructParam, err)
+}
+
+func TestStructTypesInference(t *testing.T) {
+	d, err := getStructTypes(*&dest{})
+	assert.NoError(t, err)
+	assert.EqualValues(t, map[string]string{
+		"param1": "int",
+		"param2": "bool",
+		"param3": "string",
+	}, d)
+}
+
+func TestStructTypesInferenceReturnsErrOnNoJsonTags(t *testing.T) {
+	_, err := getStructTypes(struct {
+		Param1 int
+	}{})
+	assert.ErrorIs(t, err, ErrWrongJsonTag)
+}
+
+func TestStructTypesInferenceReturnsErrOnMalformedJsonTag(t *testing.T) {
+	_, err := getStructTypes(struct {
+		Param1 int `json:"test,omitempty"`
+	}{})
+	assert.ErrorIs(t, err, ErrWrongJsonTag)
+}
+
+func TestPackRequestPathVariables(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/test/1234", nil)
+	req = mux.SetURLVars(req, map[string]string{
+		"param1": "1234",
+		"param2": "false",
+		"param3": "test",
+	})
+
+	d := &dest{}
+	err := PackRequestPathVariables(req, d)
+	assert.NoError(t, err)
+}
+
+func TestPackRequestPathVariablesFailsWithoutPointer(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/test/1234", nil)
+	req = mux.SetURLVars(req, map[string]string{
+		"param1": "1234",
+		"param2": "false",
+		"param3": "test",
+	})
+
+	d := dest{}
+	err := PackRequestPathVariables(req, d)
+	assert.ErrorIs(t, err, ErrNonPointerType)
 }
